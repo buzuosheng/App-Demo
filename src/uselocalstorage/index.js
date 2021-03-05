0cab3b93a4f9d3b2aca27d14c9c9f31d43863594
wangqi <1015350043@qq.com>
2021-03-05 18:07:33 +0800

    style: update uselocalstorage demo

--- src/uselocalstorage/index.js
+++ src/uselocalstorage/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import Helmet from "react-helmet";
 
 const UseLocalStorage = () => {
-  const [item, setItem] = useLocal('name')
+  const [item, setItem] = useLocal('name', { age: '1000' })
   return (
     < div className="use-localstorage" >
       <Helmet>
@@ -13,6 +13,8 @@ const UseLocalStorage = () => {
         </title>
       </Helmet>
       <h1>For example: set the key of localstorage to 'name' </h1>
+      <h2>default age in this case: 1s</h2>
+      <h2>default prefix: Prefix:</h2>
       <div>
         <label>
           name:{' '}
