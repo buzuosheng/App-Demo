40538742d0b41bccf06aac712c3e3518db52b133
wangqi <1015350043@qq.com>
2021-04-15 00:46:22 +0800

    style: add tailwindcss

--- src/loading/Loading.jsx
+++ src/loading/Loading.jsx
@@ -1,31 +1,49 @@
 import { Loading } from "@buzuosheng/loading";
-import "./loading.css";
 import Helmet from "react-helmet";
 
 const LdComponent = () => {
   return (
-    <div>
+    <div className="m-8 p-16 border rounded-xl bg-gray-50">
       <Helmet>
         <title>loading demo</title>
       </Helmet>
-      <div className="header">
-        <h1>8种loading样式</h1>
+      <div className="text-3xl text-center text-green-400">
+        8种loading样式
       </div>
-      <div className="loading-wrap">
-        <Loading type="Oval" color="#1890ff" hegiht={40} width={40} />
-        <Loading type={"LittleCircle"} />
-        <Loading type={"HalfCircle"} />
-        <Loading type={"Audio"} />
-        <Loading type={"Rain"} />
-        <Loading type={"Stretch"} />
-        <Loading type={"Flicker"} />
-        <Loading type={"StretchFlicker"} />
+      <div className="flex flex-row flex-wrap justify-center mt-8">
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type="Oval" color="#1890ff" hegiht={40} width={40} />
+        </div>
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'LittleCircle'} />
+        </div>
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'HalfCircle'} />
+        </div>
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'Audio'} />
+        </div>
+        <dvi className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'Rain'} />
+        </dvi>
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'Stretch'} />
+        </div>
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'Flicker'} />
+        </div>
+        <div className="w-52 mb-4 mr-4 p-4 flex justify-center items-center">
+          <Loading type={'StretchFlicker'} />
+        </div>
       </div>
-      <div className="footer">
-        <a href="https://github.com/buzuosheng/my-react-loading">源码</a>
+      <div className="text-center text-2xl mt-8">
+        Created by
+        <a href="https://github.com/buzuosheng/my-react-loading">
+          <span className="text-green-400 hover:text-green-500"> buzusheng</span>
+        </a>
       </div>
     </div>
-  );
+  )
 };
 
 export default LdComponent;
--- src/uselocalstorage/index.js
+++ src/uselocalstorage/index.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import { useLocalStorage } from '@buzuosheng/use-localstorage'
-import './index.css'
 import Helmet from "react-helmet";
 
 const UseLocalStorage = () => {
-  const [item, setItem] = useLocalStorage('name', { age: '1000' })
+  const [item, setItem] = useLocalStorage('name', { age: '5000' })
   return (
-    < div className="use-localstorage" >
+    < div className="m-8 p-16 border rounded-xl bg-gray-50" >
       <Helmet>
         <title>
           useLocalStorage demo
         </title>
       </Helmet>
-      <h1>For example: set the key of localstorage to 'name' </h1>
-      <h2>default age in this case: 1s</h2>
-      <h2>default prefix: Prefix:</h2>
-      <div>
-        <label>
-          name:{' '}
-          <input
-            type="text"
-            placeholder="Enter your name"
-            value={item}
-            onChange={e => setItem(e.target.value)}
-          />
-        </label>
+      <p className="text-center text-2xl">For example: set the key of localstorage to <span className="text-green-400">name</span> </p>
+      <p className="mt-4 text-center text-2xl">default age in this case: <span className="text-green-400">5s</span></p>
+      <p className="mt-4 text-center text-2xl">default prefix: <span className="text-green-400">Prefix:</span></p>
+      <div className="mt-4 text-center text-2xl">
+        <span>name:</span>
+        <input
+          className="ml-4 py-1 px-2 outline-none border rounded-lg"
+          placeholder="Enter your name"
+          value={item}
+          onChange={e => setItem(e.target.value)}
+        />
+      </div>
+      <div className="mt-4 text-center text-2xl">
+        <span>set the value to an object</span>
+        <button className="w-20 border rounded-lg focus:outline-none bg-green-200 text-white ml-4 p-1" onClick={() => setItem({})}>Click</button>
       </div>
     </ div>
   )
