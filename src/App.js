e5697d69508598cc90e26cd0dcd1116f80a63681
wangqi <1015350043@qq.com>
2021-09-18 16:28:28 +0800

    re clock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import LdComponet from "./loading/Loading";
 import UseLocalStorage from './uselocalstorage/index'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Clock from "./clock";
 
 const App = () => {
   return (
@@ -9,7 +8,6 @@ const App = () => {
       <Router>
         <Route path='/loading' component={LdComponet} />
         <Route path='/uselocalstorage' component={UseLocalStorage} />
-        <Route path='/clock' component={Clock} />
       </Router>
     </div>
   )
