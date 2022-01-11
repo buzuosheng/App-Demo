75507e29a81b8307e458a8a6613964f7b235def7
wangqi <1015350043@qq.com>
2022-01-11 16:38:19 +0800

    add route in homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import LdComponet from "./loading/Loading";
 import UseLocalStorage from './uselocalstorage/index'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -8,6 +8,10 @@ const App = () => {
       <Router>
         <Route path='/loading' component={LdComponet} />
         <Route path='/uselocalstorage' component={UseLocalStorage} />
+        <div className="flex flex-col items-center justify-center text-green-400 text-xl font-semibold">
+          <p className="mt-4"><Link to='/loading'>loading</Link></p>
+          <p className="mt-4"><Link to='/uselocalstorage'>uselocalstorage</Link></p>
+        </div>
       </Router>
     </div>
   )
